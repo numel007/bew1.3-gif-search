f4db372ea216cc2a74c81339230d794e0c6c891a
Jackie <taekwondo.chan@gmail.com>
2021-03-30 16:19:47 -0700

    add gif search functionality

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 // Require Libraries
+require('dotenv').config();
 const express=require('express');
+const Tenor = require("tenorjs").client({
+    "Key": process.env.API_KEY,
+    "Filter":"high",
+    "Locale":"en_US"
+});
 
 // App Setup
 const app=express();
@@ -12,8 +18,15 @@ app.set('view engine', 'handlebars');
 
 // Routes
 app.get('/', (req, res) => {
-    console.log(req.query)
-    res.render('home')
+    var term = ""
+    if (req.query.term) {
+        term = req.query.term
+    }
+
+    Tenor.Search.Query(term,"10").then(response => {
+        const gifs = response;
+        res.render('home', {gifs})
+    }).catch(console.error);
 })
 
 app.get('/greetings/:name', (req, res) => {
